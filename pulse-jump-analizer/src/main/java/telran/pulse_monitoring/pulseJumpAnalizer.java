401ccd7f21e715a47102cac22d325d87aabad844
Kostantin S <kon@mail.com>
2025-11-30 19:28:29 +0200

    first commit cw-42

---
+++ pulse-jump-analizer/src/main/java/telran/pulse_monitoring/pulseJumpAnalizer.java
@@ -0,0 +1,39 @@
+package telran.pulse_monitoring;
+
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
+
+public class pulseJumpAnalizer {
+    private static final int DEFAULT_TRESHOLD = 90;
+	 private static final int TRESHOLD = initTreshold();
+
+	 public void handleRequest(final DynamodbEvent event, final Context context) {
+	        LambdaLogger logger = context.getLogger();
+	        	event.getRecords().forEach(rec -> {
+	        		Map<String, AttributeValue>map = rec.getDynamodb().getNewImage();
+	        		int patientId = Integer.parseInt(map.get("patientId").getN()) ;
+	        		long timestamp = Long.parseLong(map.get("timestamp").getN());
+	        		int pulse = Integer.parseInt(map.get("pulse").getN());
+	        		
+	        		if(pulse >=TRESHOLD) {
+	        			logger.log(String.format("Warning! Pulse level higher then %d: PatientId: %d, timestamp: %d, pulse: %d\n",
+	        					TRESHOLD, patientId, timestamp, pulse));
+	        		}
+	        	});
+	    }
+
+	 private static int initTreshold() {
+		String value = System.getenv("TRESHOLD");
+		if(value == null)
+			return DEFAULT_TRESHOLD;
+		try {
+			return Integer.parseInt(value);
+		} catch (NumberFormatException e) {
+			return DEFAULT_TRESHOLD;
+		}
+	 }
+}
